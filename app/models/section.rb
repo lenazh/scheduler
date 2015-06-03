f0fee013a4d5c543d4f5698cb110d648b4b10e87
lenazh <lena.zhivun@berkeley.edu>
2015-06-03 18:42:20 -0300

    Validations for sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,14 @@
 class Section < ActiveRecord::Base
   validates :name, presence: true
   validates :room, presence: true
-  validates :name, uniquness: { scope: :class_id}
+  validates :name, uniqueness: { scope: :class_id}
   validate :cannot_end_before_it_starts
 
   def cannot_end_before_it_starts
-    
+    if start_time > end_time
+      msg = "can't end before it starts"
+      errors.add :end_time, msg
+      errors.add :start_time, msg
+    end
   end
 end
