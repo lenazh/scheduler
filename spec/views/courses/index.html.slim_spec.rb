93c9c23bdfb99fc53db26ce80f0b80aec2f3e8b7
lenazh <lena.zhivun@berkeley.edu>
2015-06-12 17:43:25 -0300

    Updated the view with bootstrap classes

--- spec/views/courses/index.html.slim_spec.rb
+++ spec/views/courses/index.html.slim_spec.rb
@@ -4,4 +4,15 @@ require 'helpers/json_format_helper'
 describe "courses/index" do
   let (:model_class) { Course }
   it_behaves_like 'a JSON index view:'
+
+  it "reports creation date for each course" do
+    my_courses = [create(:course), create(:another_course)]
+    assign(:courses, my_courses)
+    render
+    result = JSON.parse rendered
+    result.each do |entry|
+      expect(entry['created_at']).to_not be_nil
+    end
+  end
+
 end
