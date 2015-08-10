1c871364bfcf94bf4b898eecb40f65f906b13702
lenazh <lena.zhivun@berkeley.edu>
2015-08-10 16:04:15 -0300

    Changed default URL for Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
     authentication: :plain
   }
 
+  config.action_mailer.default_url_options = { host: 'gsi-scheduler.herokuapp.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
